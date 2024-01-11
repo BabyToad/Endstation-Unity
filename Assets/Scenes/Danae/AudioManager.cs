8e3cade29ade79e6cc7ce6b98e8fd360ee375892
DanaGuen <d.leguennic@gmail.com>
2024-01-11 09:56:40 +0100

    audiomanager

--- Assets/Scenes/Danae/AudioManager.cs
+++ Assets/Scenes/Danae/AudioManager.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static AudioManager instance {get; private set;}
+
+     //Volume controls
+
+    [Header("Volume")]
+    [Range(0,1)]
+    public float masterVolume=1;
+    [Range(0,1)]
+
+    public float musicVolume=1;
+    [Range(0,1)]
+
+    public float sfxVolume=1;
+    [Range(0,1)]
+
+    private Bus masterBus;
+    private Bus musicBus;
+    private Bus sfxBus;
+
+    private void Awake()
     {
-        
+        if (instance!= null)
+    {
+        Debug.LogError("Found more than one AudioManager instance in the scene.");
+    }
+        instance=this;
+
+        masterBus = RuntimeManager.GetBus("bus:/");
+        musicBus = RuntimeManager.GetBus("bus:/Music");
+        sfxBus = RuntimeManager.GetBus("bus:/SFX");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
+
+
+
+
 }
