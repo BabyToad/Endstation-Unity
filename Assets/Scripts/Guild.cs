904671f2fc2b2dfc7176eab7883c8db75022062a
DanaGuen <d.leguennic@gmail.com>
2024-01-11 16:03:20 +0100

    sound prefab update

--- Assets/Scripts/Guild.cs
+++ Assets/Scripts/Guild.cs
@@ -120,6 +120,7 @@ public class Guild : MonoBehaviour
         }
        
         OnEndCycle();
+        AudioManager.instance.PlayOneShot(FMODEvents.instance._uiClick);
         AudioManager.instance.PlayOneShot(FMODEvents.instance._endTurn);
         // tick clocks
     }
